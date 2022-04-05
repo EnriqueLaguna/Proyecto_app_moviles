48898dc0ddce2c062e67b8800fe22cb8864d5d72
58639436 <58639436+EnriqueLaguna@users.noreply.github.com>
2022-04-05 17:04:34 -0500

    Autenticacion de usuarios, siiiii

--- lib/Login/login.dart
+++ lib/Login/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:proyecto_app_moviles/Login/auth/bloc/auth_bloc.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -13,71 +15,75 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(children: [
-        const Text("Nombre de la aplicación", style: TextStyle(fontWeight: FontWeight.bold),),
-        TextFormField(
-          decoration: InputDecoration(
-            border: borde,
-            enabledBorder: borde,
-            focusedBorder: borde,
-            labelText: "Correo electrónico",
-            floatingLabelStyle: TextStyle(color: Colors.green),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(children: [
+          const Text("Nombre de la aplicación", style: TextStyle(fontWeight: FontWeight.bold),),
+          TextFormField(
+            decoration: InputDecoration(
+              border: borde,
+              enabledBorder: borde,
+              focusedBorder: borde,
+              labelText: "Correo electrónico",
+              floatingLabelStyle: TextStyle(color: Colors.green),
+            ),
           ),
-        ),
-        TextFormField(
-          controller: passwordController,
-          obscureText: true,
-          enableSuggestions: false,
-          autocorrect: false,
-          decoration: InputDecoration(
-            border: borde,
-            enabledBorder: borde,
-            focusedBorder: borde,
-            labelText: "Contraseña",
-            //suffix: IconButton(onPressed: (){}, icon: const Icon(Icons.remove_red_eye)),
-            floatingLabelStyle: TextStyle(color: Colors.green),
+          TextFormField(
+            controller: passwordController,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: InputDecoration(
+              border: borde,
+              enabledBorder: borde,
+              focusedBorder: borde,
+              labelText: "Contraseña",
+              //suffix: IconButton(onPressed: (){}, icon: const Icon(Icons.remove_red_eye)),
+              floatingLabelStyle: TextStyle(color: Colors.green),
+            ),
           ),
-        ),
-        MaterialButton(
-          onPressed: (){},
-          child: Text("¿No tienes cuenta? Regístrate aquí", style: TextStyle(color: Colors.lightGreen, fontWeight: FontWeight.bold),)
-        ),
-        SizedBox(
-          width: MediaQuery.of(context).size.width*0.75,
-          child: Column(mainAxisSize: MainAxisSize.min, mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
-            ElevatedButton(
-              onPressed: (){}, 
-              child: const ListTile(title: Text("Iniciar Sesión", textAlign: TextAlign.center, style: TextStyle(color: Colors.white),)),
-              style: ButtonStyle(
-                shape: MaterialStateProperty.all(
-                  const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(150),),
-                  )
+          MaterialButton(
+            onPressed: (){},
+            child: Text("¿No tienes cuenta? Regístrate aquí", style: TextStyle(color: Colors.lightGreen, fontWeight: FontWeight.bold),)
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width*0.75,
+            child: Column(mainAxisSize: MainAxisSize.min, mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
+              ElevatedButton(
+                onPressed: (){}, 
+                child: const ListTile(title: Text("Iniciar Sesión", textAlign: TextAlign.center, style: TextStyle(color: Colors.white),)),
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(
+                    const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(150),),
+                    )
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 10,),
-            TextButton(
-              onPressed: (){},
-              style: ButtonStyle(
-                shape: MaterialStateProperty.all(
-                  RoundedRectangleBorder(
-                    borderRadius: const BorderRadius.all(Radius.circular(150),),
-                    side: BorderSide(color: Theme.of(context).primaryColor,width: 2,style: BorderStyle.solid),
-                  )
+              const SizedBox(height: 10,),
+              TextButton(
+                onPressed: (){
+                  BlocProvider.of<AuthBloc>(context).add(GoogleAuthEvent());
+                },
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: const BorderRadius.all(Radius.circular(150),),
+                      side: BorderSide(color: Theme.of(context).primaryColor,width: 2,style: BorderStyle.solid),
+                    )
+                  ),
                 ),
+                child: const ListTile(
+                  leading: Icon(Icons.g_mobiledata, size: 36,),
+                  title: Text("Iniciar con Google", style: TextStyle(color: Colors.grey), textAlign: TextAlign.start,),
+                )
               ),
-              child: const ListTile(
-                leading: Icon(Icons.g_mobiledata, size: 36,),
-                title: Text("Iniciar con Google", style: TextStyle(color: Colors.grey), textAlign: TextAlign.start,),
-              )
-            ),
-          ],),
-        )
-      ],
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,),
+            ],),
+          )
+        ],
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:proyecto_app_moviles/HomePage/homePage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:proyecto_app_moviles/Login/auth/bloc/auth_bloc.dart';
+import 'package:proyecto_app_moviles/Login/login.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AuthBloc()..add(VerifyAuthEvent())
+        )
+      ], 
+      child: MyApp()
+    )
+  );
 }
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      theme: ThemeData(primarySwatch: Colors.lightGreen),
-      home: HomePage()
-    );
+        title: 'Material App',
+        theme: ThemeData(primarySwatch: Colors.lightGreen),
+        home: BlocConsumer<AuthBloc, AuthState>(
+          listener: (context, state) {
+            if (state is AuthErrorState){
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text("Favor de autenticarse"))
+              );
+            }
+          },
+          builder: (context, state) {
+            if(state is AuthSuccessState){
+              return HomePage();
+            } else if(state is UnAuthState || state is AuthErrorState){
+              return LoginPage();
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ));
   }
 }
